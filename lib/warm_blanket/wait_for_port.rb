e2881fcedf643a6c2061a60324f1ef5cadb2f0fc
Ivo Anjo <ivo.anjo@talkdesk.com>
2017-06-28 17:38:29 +0100

    Add first stab at WaitForPort class
    This class waits for a given port to be open on the target machine.

---
+++ lib/warm_blanket/wait_for_port.rb
@@ -0,0 +1,50 @@
+require 'socket'
+
+module WarmBlanket
+  # Waits for given port to be available
+  class WaitForPort
+
+    InvalidPort = Class.new(StandardError)
+
+    private
+
+    attr_reader :hostname
+    attr_reader :port
+    attr_reader :logger
+    attr_reader :tries_limit
+
+    public
+
+    def initialize(hostname: 'localhost', port:, tries_limit: 90, logger: WarmBlanket.config.logger)
+      raise "Invalid port (#{port.inspect})" unless (1...2**16).include?(port)
+
+      @hostname = hostname
+      @port = port
+      @logger = logger
+      @tries_limit = tries_limit
+    end
+
+    def call
+      logger.debug "Waiting for #{hostname}:#{port} to be available"
+
+      tries = 0
+
+      while true
+        socket = nil
+        begin
+          socket = TCPSocket.new(hostname, port)
+          logger.debug "Service at #{hostname}:#{port} is up"
+          return true
+        rescue StandardError => e
+          logger.debug "Exception while waiting for port to be available #{e.class}: #{e.message}"
+        ensure
+          socket&.close
+        end
+
+        tries += 1
+        return false if tries >= tries_limit
+        sleep 1
+      end
+    end
+  end
+end
---
+++ spec/integration/wait_for_port_spec.rb
@@ -0,0 +1,81 @@
+require 'warm_blanket/wait_for_port'
+require 'socket'
+
+RSpec.describe WarmBlanket::WaitForPort do
+  let(:port) { (2**10...2**16).to_a.sample }
+  let(:default_hostname) { 'localhost' }
+  let(:optional_arguments) { {} }
+
+  subject { described_class.new(port: port, **optional_arguments) }
+
+  describe '.call' do
+    let(:call) { subject.call }
+
+    context 'when service is available' do
+      let!(:open_socket) { TCPServer.open(port) }
+      let!(:server_background_thread) { Thread.new { open_socket.accept } }
+
+      after do
+        server_background_thread.join
+        open_socket.close
+      end
+
+      it do
+        expect(call).to be true
+      end
+    end
+
+    context 'when service is not available' do
+      let(:tries_limit) { 3 }
+      let(:optional_arguments) { {tries_limit: tries_limit} }
+
+      before do
+        allow(subject).to receive(:sleep)
+      end
+
+      it do
+        expect(call).to be false
+      end
+
+      it 'retries tries_limit times' do
+        expect(TCPSocket).to receive(:new)
+          .with(default_hostname, port).exactly(tries_limit).times.and_call_original
+
+        call
+      end
+
+      it 'sleeps one second between tries' do
+        should_sleep_now = false
+
+        allow(TCPSocket).to receive(:new) do
+          should_sleep_now = true
+          raise Errno::ECONNREFUSED
+        end
+
+        expect(subject).to receive(:sleep).with(1).exactly(tries_limit - 1).times do
+          expect(should_sleep_now).to be true
+          should_sleep_now = false
+        end
+
+        call
+      end
+
+      context 'when service becomes available after the n-th try' do
+        it do
+          attempts = 0
+
+          allow(TCPSocket).to receive(:new) do
+            attempts += 1
+            if attempts > 2
+              instance_double(TCPSocket, close: nil)
+            else
+              raise Errno::ECONNREFUSED
+            end
+          end
+
+          expect(call).to be true
+        end
+      end
+    end
+  end
+end
